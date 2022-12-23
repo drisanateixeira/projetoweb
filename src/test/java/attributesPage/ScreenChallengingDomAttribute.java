1f9217a724cd80ad93e7932a8fd3d5b4fb916b89
drisanateixeira <48416450+drisanateixeira@users.noreply.github.com>
2022-12-23 09:41:34 -0300

    Update ScreenChallengingDomAttribute.java

--- src/test/java/attributesPage/ScreenChallengingDomAttribute.java
+++ src/test/java/attributesPage/ScreenChallengingDomAttribute.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.By;
 
 public class ScreenChallengingDomAttribute {
 
-	protected int variavel;
-	
 	protected static final By quxBotao = By.xpath("//*[@class='button']");
 	protected static final By barBotao = By.xpath("//*[contains(@class, 'button alert')]");
 	protected static final By quxSuccessBotao = By.xpath("//a[contains(@class, 'button success')]");
